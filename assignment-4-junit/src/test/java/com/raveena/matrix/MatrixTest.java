68f8c9340c0c1ff5e0eb0896d505d5dbc2fbf629
raveena25711 <raveenanalla01@gmail.com>
2022-02-04 10:28:59 +0530

    assignment-4 / raveena / updated the changes

--- assignment-4-junit/src/test/java/com/raveena/matrix/MatrixTest.java
+++ assignment-4-junit/src/test/java/com/raveena/matrix/MatrixTest.java
@@ -13,82 +13,53 @@ class MatrixTest {
     void testAddingOfTwoMatrices() {
         Number[][] actual = Matrix.getAddingOfTwoMatrices(matrixA,matrixB);
         Number[][] expected={{2.0, 4.0, 6.0 }, {4.0, 6.0, 8.0 }, {4.0, 6.0, 8.0}};
+        assertNotNull(actual);
         assertArrayEquals(expected,actual);
         assertEquals(matrixA.length,expected.length);
         assertEquals(matrixA[0].length,expected[0].length);
-        assertNotNull(actual);
-        if(matrixA.length>expected.length || matrixA.length<expected.length){
-            fail("rows are not matching");
-        }
-        if(matrixA[0].length>expected[0].length || matrixA[0].length<expected[0].length){
-            fail("columns are not matching");
-        }
+
     }
 
     @Test
     void testSubstractingOfTwoMatrices() {
         Number[][] actual = Matrix.getSubstractingOfTwoMatrices(matrixA,matrixB);
         Number[][] expected={{0.0, 0.0, 0.0 }, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}};
+        assertNotNull(actual);
         assertArrayEquals(expected,actual);
         assertArrayEquals(expected,actual);
         assertEquals(matrixA.length,expected.length);
         assertEquals(matrixA[0].length,expected[0].length);
-        assertNotNull(actual);
-        if(matrixA.length>expected.length || matrixA.length<expected.length){
-            fail("rows are not matching");
-        }
-        if(matrixA[0].length>expected[0].length || matrixA[0].length<expected[0].length){
-            fail("columns are not matching");
-        }
     }
 
     @Test
     void testMultiplicationOfTwoMatrices() {
         Number[][] actual = Matrix.getMultiplicationOfTwoMatrices(matrixA,matrixB);
         Number[][] expected={{11.0, 17.0, 23.0}, {16.0, 25.0, 34.0 }, {16.0, 25.0, 34.0 }};
+        assertNotNull(actual);
         assertArrayEquals(expected,actual);
         assertArrayEquals(expected,actual);
         assertEquals(matrixA.length,expected.length);
         assertEquals(matrixB[0].length,expected[0].length);
-        assertNotNull(actual);
-        if(matrixA.length>expected.length || matrixA.length<expected.length){
-            fail("rows are not matching");
-        }
-        if(matrixB[0].length>expected[0].length || matrixB[0].length<expected[0].length){
-            fail("columns are not matching");
-        }
     }
 
     @Test
     void testscalarMatrix() {
         Number[][] actual = Matrix.getscalarMatrix(matrixA);
         Number[][] expected={{2.0, 4.0, 6.0 }, {4.0, 6.0, 8.0 }, {4.0, 6.0, 8.0 }};
+        assertNotNull(actual);
         assertArrayEquals(expected,actual);
         assertArrayEquals(expected,actual);
         assertEquals(matrixA.length,expected.length);
         assertEquals(matrixA[0].length,expected[0].length);
-        assertNotNull(actual);
-        if(matrixA.length>expected.length || matrixA.length<expected.length){
-            fail("rows are not matching");
-        }
-        if(matrixA[0].length>expected[0].length || matrixA[0].length<expected[0].length){
-            fail("columns are not matching");
-        }
     }
 
     @Test
     void testTransposeOfTwoMatrices() {
         Number[][] actual = Matrix.getTransposeMatrix(matrixA);
         Number[][] expected={{1.0, 2.0, 2.0}, {2.0, 3.0, 3.0},{ 3.0, 4.0, 4.0}};
-        if(matrixA.length>expected.length || matrixA.length<expected.length){
-            fail("rows are not matching");
-        }
-        if(matrixA[0].length>expected[0].length || matrixA[0].length<expected[0].length){
-            fail("columns are not matching");
-        }
+        assertNotNull(actual);
         assertArrayEquals(expected,actual);
         assertEquals(matrixA.length,expected.length);
         assertEquals(matrixA[0].length,expected[0].length);
-        assertNotNull(actual);
     }
 }
